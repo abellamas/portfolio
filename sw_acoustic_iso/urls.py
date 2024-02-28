af9deea6eb99d15a787bc2775a0a5b6c8038cd8f
Abel Lamas <abellamasm@gmail.com>
2024-02-28 01:34:06 -0300

    configuracion de anchor links

--- sw_acoustic_iso/urls.py
+++ sw_acoustic_iso/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='sw_acoustic_iso'),
     path('export_excel/', views.export_excel, name='export_excel')
 ]
